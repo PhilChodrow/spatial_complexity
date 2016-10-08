07779dc4adaa74e3d1417bdfd6e42891c62ed37c
PhilChodrow <pchodrow@gmail.com>
2016-10-08 00:32:07 -0400

    automated aggregation of racial categories

--- 1_get_states.R
+++ 1_get_states.R
@@ -3,7 +3,7 @@ library(acs, quietly = TRUE)
 library(dplyr, quietly = TRUE)
 library(tidyr, quietly = TRUE)
 library(stringr, quietly = TRUE)
-
+library(readr)
 
 # get list of states
 
@@ -13,7 +13,6 @@ cities$state <- word(cities$state, -1)
 if(!dir.exists('data')){
 	dir.create('data')
 }
-
 
 if(!dir.exists('data/states')){
 	dir.create('data/states')
@@ -35,34 +34,25 @@ f <- function(state){
 	
 	race <- cbind(data.frame(race@geography),
 				  data.frame(race@estimate)) %>%
-		tbl_df() %>%
+		tbl_df() 
+	
+	race_lookup <- read_csv('assumptions/race_lookup.csv')
+	
+	race <- race %>% 
+		gather(key = 'original_race', value = 'n', -(NAME:blockgroup)) %>% 
+		left_join(race_lookup, by = c('original_race' = 'original')) %>% 
+		group_by(NAME, state, county, tract, blockgroup, aggregated) %>% 
+		summarise(n = sum(n)) %>% 
+		ungroup() %>% 
+		filter(!is.na(aggregated)) %>% 
+		spread(key = aggregated, value = n, fill = 0)
+	
+	race <- race %>% 
 		mutate(GEOID = paste0(str_pad(state, 2, 'left', pad = '0'),
 							  str_pad(county, 3, 'left', pad = '0'),
 							  str_pad(tract, 6, 'left', pad = '0'),
 							  str_pad(blockgroup, 1, 'left', pad = '0')))
 	
-	race$Hispanic <- race[names(race)[!grepl('.Not.Hispanic.or.Latino.',names(race))]] %>%
-		select(-(`NAME`:`Hispanic.or.Latino.by.Race..Hispanic.or.Latino.`)) %>%
-		select(-GEOID) %>%
-		rowSums()
-	
-	others <- c('Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..American.Indian.and.Alaska.Native.alone',
-				'Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..Native.Hawaiian.and.Other.Pacific.Islander.alone',
-				'Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..Some.other.race.alone',
-				'Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..Two.or.more.races.',
-				'Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..Two.or.more.races..Two.races.including.Some.other.race',
-				'Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..Two.or.more.races..Two.races.excluding.Some.other.race..and.three.or.more.races')
-	
-	race$Other <- race[others] %>% rowSums()
-	
-	race <- race %>%
-		select(Hispanic, Other,
-			   White = Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..White.alone,
-			   Black = Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..Black.or.African.American.alone,
-			   Asian = Hispanic.or.Latino.by.Race..Not.Hispanic.or.Latino..Asian.alone,
-			   GEOID) %>%
-		mutate(total = Hispanic + White + Black + Asian + Other)
-
 	tracts <- tigris::block_groups(state = state, county = counties, cb=TRUE)
 	tracts@data$area <- tracts@data$ALAND / 1000^2
 	
